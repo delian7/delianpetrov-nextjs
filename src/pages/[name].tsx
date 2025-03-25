c98951eccf47e17cc0fae277c7395a242b57a431
Delian Petrov <delian7@gmail.com>
2025-03-25 16:37:42 +0000

    on redirect, display the og properties

--- src/pages/[name].tsx
+++ src/pages/[name].tsx
@@ -1,58 +1,60 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { useAlertContext } from '../context/alertContext';
-import { Box } from '@chakra-ui/react';
+import { GetServerSideProps } from "next";
+import Head from "next/head";
 
-const RedirectPage = () => {
-  const router = useRouter();
-  const { name } = router.query; // Access the dynamic "name" parameter from the URL
-  const { onOpen } = useAlertContext();
+interface NotionData {
+  title: string;
+  description: string;
+  image: string;
+}
 
-  useEffect(() => {
-    const fetchAndRedirect = async () => {
-      if (!name) return;
-
-      try {
-        const response = await fetch(
-          `https://qpqyy5wg42qcon34ph6mhljct40wtmpl.lambda-url.us-east-2.on.aws/?name=${name}`
-        );
-
-        if (response.ok) {
-          const data = await response.json();
-
-          if (data.url) {
-            router.push(data.url);
-            onOpen('error', 'The short link was not found');
-          } else {
-            router.push('/'); // Redirect to homepage if URL is not found
-            onOpen('error', 'The short link was not found');
-          }
-        } else {
-          router.push('/'); // Handle error and redirect to homepage
-          onOpen('error', 'The short link was not found');
-        }
-      } catch (error) {
-        console.error('Error fetching URL:', error);
-        router.push('/');
-      }
-    };
-
-    fetchAndRedirect();
-  }, [name, onOpen, router]); // Re-run effect when "name" changes
+export default function DynamicPage({ notionData }: { notionData: NotionData }) {
+  if (!notionData) {
+    return <div>Redirecting...</div>;
+  }
 
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      minHeight="100vh"
-      background={'lavender'}
-    >
-      <div className="flex justify-center items-center h-screen">
-        <div className="border-t-4 border-blue-500 border-solid w-16 h-16 rounded-full animate-spin"></div>
+    <>
+      <Head>
+        <title>{notionData.title}</title>
+        <meta property="og:title" content={notionData.title} />
+        <meta property="og:description" content={notionData.description} />
+        <meta property="og:image" content={notionData.image} />
+      </Head>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="w-16 h-16 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin"></div>
       </div>
-    </Box>
-  );
+    </>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { name } = context.params!;
+  const notionData = await fetchNotionData(name as string);
+
+  if (!notionData) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    redirect: {
+      destination: notionData.url, // Redirect to the URL from Notion data
+      permanent: false,
+    },
+  };
 };
 
-export default RedirectPage;
+async function fetchNotionData(name: string) {
+  const response = await fetch(`https://qpqyy5wg42qcon34ph6mhljct40wtmpl.lambda-url.us-east-2.on.aws/?name=${name}`);
+  const data = await response.json();
+
+  if (!data["url"]) {
+    return null;
+  }
+
+  return data;
+}
