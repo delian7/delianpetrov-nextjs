c52084795f49a1ab79937f5fc8b40b6fd9a236ce
Delian Petrov <delian7@gmail.com>
2026-05-29 22:53:08 -0700

    contact form fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -504,7 +504,7 @@ function ContactForm() {
     const data = Object.fromEntries(new FormData(form));
 
     try {
-      await fetch("/api/contact", {
+      await fetch("https://dhdmjop4ywcsbgyiwncrg4amdi0ksjqm.lambda-url.us-east-2.on.aws", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
